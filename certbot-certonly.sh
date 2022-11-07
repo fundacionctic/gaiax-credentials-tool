178633cdd01d5251ea21591c162de8e8e7764ee4
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2022-11-07 17:37:18 +0100

    Add certs target

--- certbot-certonly.sh
+++ certbot-certonly.sh
@@ -2,9 +2,24 @@
 
 set -ex
 
-: "${PUBLIC_CERTBOT_PORT:?}"
+: "${CERTBOT_PUBLIC_PORT:?}"
+: "${CERTBOT_EMAIL:?}"
+: "${CERTBOT_DOMAIN:?}"
 
 docker run -it --rm \
-    -p ${PUBLIC_CERTBOT_PORT}:80 \
+    -p ${CERTBOT_PUBLIC_PORT}:80 \
     -v "$(pwd)/letsencrypt:/etc/letsencrypt" \
-    certbot/certbot certonly --standalone
+    certbot/certbot certonly --standalone -n --agree-tos --email ${CERTBOT_EMAIL} -d ${CERTBOT_DOMAIN}
+
+CERT=$(sudo cat ./letsencrypt/live/${CERTBOT_DOMAIN}/cert.pem)
+PRIVKEY=$(sudo cat ./letsencrypt/live/${CERTBOT_DOMAIN}/privkey.pem)
+FULLCHAIN=$(sudo cat ./letsencrypt/live/${CERTBOT_DOMAIN}/fullchain.pem)
+
+touch .env
+
+echo $'\n' >>.env
+echo "CERTIFICATE=\"${CERT}\"" >>.env
+echo $'\n' >>.env
+echo "PRIVATE_KEY=\"${PRIVKEY}\"" >>.env
+echo $'\n' >>.env
+echo "FULLCHAIN=\"${FULLCHAIN}\"" >>.env
